91726d4737ee7b5dac1f6281d3c919094935724f
Tom Kennedy <kennedy513@gmail.com>
2017-07-23 17:46:07 -0400

    started groomsmen blurbs

--- src/assets/bridal-party-members.ts
+++ src/assets/bridal-party-members.ts
@@ -9,9 +9,19 @@ export const bridalParty: [Member] = [
         aboutParagraph: "Emma has been by my side from the very beginning. We met at Little Sparrows Preschool when we were 3 years old and have been inseparable ever since. From awkward middle school years and high school parties to attending Villanova together and even joining the same sorority, Emma and I have been soul sisters for over 20 years. I truly would not be the person I am today if I hadn’t gone through life with Emma by my side. We have been talking about being each other's Maid of Honor since we were 12 and I couldn’t be more thrilled that that time has come!"
     },
     {
+      name: "Best Man: Pete Kennedy",
+      photoUrl: "/assets/images/bridal_party_photos/pete.jpg",
+      aboutParagraph: "Pete is my younger brother"
+    },
+    {
         name: "Bridesmaid: Kirstie Walker",
         photoUrl: "/assets/images/bridal_party_photos/Kirstie.jpg",
         aboutParagraph: "Kirstie AKA “cuzcuz” is so much more than just my cousin. She is a best friend, a confidant, and a sister to me. For the first 14 years of my life Kirstie was someone that lived far far away that I saw once a year. However, once she moved back up to NY from Florida, we quickly realized how similar we were and have been inseparable ever since. I am so grateful that she made that move so many years ago and has became such a big part of my life. Even though she is a cat person, I love her to death and I am so excited to have her in my wedding!"
+    },
+    {
+      name: "Groomsman: Mark Kennedy",
+      photoUrl: "/assets/images/bridal_party_photos/mark.jpg",
+      aboutParagraph: "Mark is my youngest brother"
     },
     {
       name: "Bridesmaid: Stephanie Viola",
@@ -19,9 +29,19 @@ export const bridalParty: [Member] = [
       aboutParagraph: "Steph and I met my freshman year in high school. I was a new kid at Sacred Heart and was experiencing catholic school life for the first time. Steph was one of my first friends at CSH and has continued to be one of the best ever since. Through the years, Steph has been my go-to person for when I need advice or to vent about something. She is one of the most open-minded and kind people that I know and I am so happy to have her as a bridesmaid!"
     },
     {
+      name: "Groomsman: Lars Farber",
+      photoUrl: "/assets/images/bridal_party_photos/lars.jpg",
+      aboutParagraph: "Lars is my friend"
+    },
+    {
       name: "Bridesmaid: Maggie Gavin",
       photoUrl: "/assets/images/bridal_party_photos/maggie.jpg",
       aboutParagraph: "Maggie and I have been friends since my freshman year in high school. While we became fast friends right off the bat, it was our shared disdain for Madame Cooley’s french class that really made us close. Maggie is very generous (as she let me copy her homework all the time) and is hands down one of the funniest people I have ever met. She never fails to make me laugh and I am thrilled to have her as part of my big day!"
+    },
+    {
+      name: "Groomsman: Dave Cusack",
+      photoUrl: "/assets/images/bridal_party_photos/dave.jpg",
+      aboutParagraph: "Dave is my friend"
     },
     {
       name: "Bridesmaid: Annie Mcdevitt",
@@ -29,9 +49,19 @@ export const bridalParty: [Member] = [
       aboutParagraph: "Annie and I have been friends since the 9th grade. She is such a fun person to be around and her happy-go-lucky attitude is absolutely contagious. I remember having to bribe my brother Wes to drive me to her house since she lived  45 minutes away from me. Through college, the distance grew even more as she spent every summer on some ranch in Colorado. No matter how far apart we were, we always managed to keep in touch and make a point to get together when she was home.I am so happy to have her back on the east coast and participating in my big day!"
     },
     {
+      name: "Groomsman: Eric Newton",
+      photoUrl: "/assets/images/bridal_party_photos/eric.jpg",
+      aboutParagraph: "Eric is my friend"
+    },
+    {
         name: "Bridesmaid: Kori Alzate",
         photoUrl: "/assets/images/bridal_party_photos/kori.jpg",
         aboutParagraph: "I met Kori in the library during 10th grade when I randomly decided to start up a conversation with the new girl who was sitting by herself. While this sounds like such a nice and noble thing to do, I am pretty sure I interrupted her while she was working and bombarded her with annoying and intrusive questions. I knew we were going to be great friends from her ability to put up with me. Kori is one of the most personable and go-with-the-flow people that I know. I am willing to look past that time that she gave me a baby bib for Christmas and am so excited for her to be part of my wedding!"
+    },
+    {
+      name: "Groomsman: Matt Creeron",
+      photoUrl: "/assets/images/bridal_party_photos/matt.jpg",
+      aboutParagraph: "Matt is my friend"
     },
     {
         name: "Bridesmaid: Shannen Gorman",
@@ -39,39 +69,9 @@ export const bridalParty: [Member] = [
         aboutParagraph: "Shannen and I were in the same sorority  pledge class at Villanova (go cats). Shannen was one of my first friends in Pi Phi and I am so happy to call her my sister. She is always up for anything, constantly has your back, and is an incredibly loyal friend. I am so excited to have her be by my side on my big day!"
     },
     {
-      name: "Best Man: Pete Kennedy",
-      photoUrl: "/assets/images/bridal_party_photos/pete.jpg",
-      aboutParagraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt quis mauris tempus gravida. Nullam a dapibus orci. Proin eget enim vitae neque mollis faucibus. Quisque ut tincidunt ante, nec efficitur turpis. Morbi pretium ex porta, efficitur dolor non, feugiat urna. Sed auctor sollicitudin nunc dapibus rutrum. Morbi nisi mi, porta ut suscipit ut, porttitor ut enim. Nam ut massa nisi. Vivamus quis luctus risus, non eleifend erat. Vivamus vestibulum sagittis lectus vitae aliquet."
-    },
-    {
-      name: "Groomsman: Mark Kennedy",
-      photoUrl: "/assets/images/bridal_party_photos/mark.jpg",
-      aboutParagraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt quis mauris tempus gravida. Nullam a dapibus orci. Proin eget enim vitae neque mollis faucibus. Quisque ut tincidunt ante, nec efficitur turpis. Morbi pretium ex porta, efficitur dolor non, feugiat urna. Sed auctor sollicitudin nunc dapibus rutrum. Morbi nisi mi, porta ut suscipit ut, porttitor ut enim. Nam ut massa nisi. Vivamus quis luctus risus, non eleifend erat. Vivamus vestibulum sagittis lectus vitae aliquet."
-    },
-    {
-      name: "Groomsman: Lars Farber",
-      photoUrl: "/assets/images/bridal_party_photos/lars.jpg",
-      aboutParagraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt quis mauris tempus gravida. Nullam a dapibus orci. Proin eget enim vitae neque mollis faucibus. Quisque ut tincidunt ante, nec efficitur turpis. Morbi pretium ex porta, efficitur dolor non, feugiat urna. Sed auctor sollicitudin nunc dapibus rutrum. Morbi nisi mi, porta ut suscipit ut, porttitor ut enim. Nam ut massa nisi. Vivamus quis luctus risus, non eleifend erat. Vivamus vestibulum sagittis lectus vitae aliquet."
-    },
-    {
-      name: "Groomsman: Dave Cusack",
-      photoUrl: "/assets/images/bridal_party_photos/dave.jpg",
-      aboutParagraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt quis mauris tempus gravida. Nullam a dapibus orci. Proin eget enim vitae neque mollis faucibus. Quisque ut tincidunt ante, nec efficitur turpis. Morbi pretium ex porta, efficitur dolor non, feugiat urna. Sed auctor sollicitudin nunc dapibus rutrum. Morbi nisi mi, porta ut suscipit ut, porttitor ut enim. Nam ut massa nisi. Vivamus quis luctus risus, non eleifend erat. Vivamus vestibulum sagittis lectus vitae aliquet."
-    },
-    {
-      name: "Groomsman: Matt Creeron",
-      photoUrl: "/assets/images/bridal_party_photos/matt.jpg",
-      aboutParagraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt quis mauris tempus gravida. Nullam a dapibus orci. Proin eget enim vitae neque mollis faucibus. Quisque ut tincidunt ante, nec efficitur turpis. Morbi pretium ex porta, efficitur dolor non, feugiat urna. Sed auctor sollicitudin nunc dapibus rutrum. Morbi nisi mi, porta ut suscipit ut, porttitor ut enim. Nam ut massa nisi. Vivamus quis luctus risus, non eleifend erat. Vivamus vestibulum sagittis lectus vitae aliquet."
-    },
-    {
       name: "Groomsman: Dan Cross",
-      photoUrl: "/assets/images/bridal_party_photos/dan.jpg", 
-      aboutParagraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt quis mauris tempus gravida. Nullam a dapibus orci. Proin eget enim vitae neque mollis faucibus. Quisque ut tincidunt ante, nec efficitur turpis. Morbi pretium ex porta, efficitur dolor non, feugiat urna. Sed auctor sollicitudin nunc dapibus rutrum. Morbi nisi mi, porta ut suscipit ut, porttitor ut enim. Nam ut massa nisi. Vivamus quis luctus risus, non eleifend erat. Vivamus vestibulum sagittis lectus vitae aliquet."
-    },
-    {
-      name: "Groomsman: Eric Newton",
-      photoUrl: "/assets/images/bridal_party_photos/eric.jpg",
-      aboutParagraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt quis mauris tempus gravida. Nullam a dapibus orci. Proin eget enim vitae neque mollis faucibus. Quisque ut tincidunt ante, nec efficitur turpis. Morbi pretium ex porta, efficitur dolor non, feugiat urna. Sed auctor sollicitudin nunc dapibus rutrum. Morbi nisi mi, porta ut suscipit ut, porttitor ut enim. Nam ut massa nisi. Vivamus quis luctus risus, non eleifend erat. Vivamus vestibulum sagittis lectus vitae aliquet."
+      photoUrl: "/assets/images/bridal_party_photos/dan.jpg",
+      aboutParagraph: "Dan is my friend"
     },
     {
       name: "Usher: Peter Merritt",
