4d0923b338fb2e905dc34b381ed78482f88a7ff5
Tom Kennedy <kennedy513@gmail.com>
2017-08-15 18:32:54 -0400

    disable fullpage js on pages other than home

--- src/app/components/bridal-party/bridal-party.component.ts
+++ src/app/components/bridal-party/bridal-party.component.ts
@@ -16,6 +16,12 @@ export class BridalPartyComponent implements OnInit {
 
 
   ngOnInit() {
+    const divToChange1 = document.getElementsByTagName('html');
+    divToChange1.item(0).className="";
+    divToChange1.item(0).style.overflow='scroll';
+    const divToChange2 = document.getElementsByTagName('body');
+    divToChange2.item(0).className="";
+    divToChange2.item(0).style.overflow='scroll'
 
   }
 
