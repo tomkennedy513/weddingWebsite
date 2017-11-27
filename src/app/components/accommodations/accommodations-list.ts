2bac6bbc06519e2bcf472065946f69315ae9d228
Merritt <merem02@ca.com>
2017-11-26 22:29:35 -0500

    accomodations info done

--- src/app/components/accommodations/accommodations-list.ts
+++ src/app/components/accommodations/accommodations-list.ts
@@ -1,13 +1,55 @@
 import { Accommodation } from "app/components/accommodations/accommodation-info/accommodation";
-import { EncodeURIComponentPipe } from 'angular-pipes/src/string/encode-uri-component.pipe';
 
 export const accommodations: [Accommodation] = [
+
     {
-        name: "Tom Kennedy",
-        photoUrl: "/assets/img/bridal_party_photos/mark.jpg",
-        aboutParagraph: "Test",
-        address: "96 Gail Ct, 11731",
-        phone: "631-707-4771",
-        website: "http://google.com"
+        name: "The Montauk Yacht Club",
+        photoUrl: "",
+        aboutParagraph: "Book your stay at our beautiful reception venue!",
+        address: "32 Star Island Rd, Montauk, NY 11954",
+        phone: "631-668-3100",
+        website: "http://www.montaukyachtclub.com/"
+    },
+    {
+        name: "The Montauk Manor",
+        photoUrl: "",
+        aboutParagraph:"Gorgeous Manor on a hill with beautiful views.",
+        address:"236 Edgemere St, Montauk, NY 11954",
+        phone: "631-668-4400",
+        website:"http://www.montaukmanor.com/"
+
+    },
+    {
+        name:"Snug Harbor Motel",
+        photoUrl:"",
+        aboutParagraph:"A short 5 minute walk from the venue! Our room block will be available for booking in the spring.",
+        address: "3 Star Island Rd, Montauk, NY 11954",
+        phone: "631-668-2860",
+        website:"http://www.montauksnugharbor.com/"
+    
+    },
+    {
+        name:"Sun N' Sound",
+        photoUrl:"",
+        aboutParagraph:"",
+        address:"22 Soundview Dr, Montauk, NY 11954",
+        phone:"631-668-2212",
+        website:"https://www.duneresorts.com/resorts/sun-n-sound/"
+    },
+    {
+        name:"Harborside Resort Motel",
+        photoUrl:"",
+        aboutParagraph:"A short 10 Minute walk from our venue!",
+        address:"371 W Lake Dr, Montauk, NY 11954",
+        phone:"631-668-2511",
+        website:"http://www.montaukharborside.com/"
+    },
+    {
+        name:"Airbnb",
+        photoUrl:"",
+        aboutParagraph:"",
+        address:"",
+        phone:"",
+        website:""
     }
-]
+
