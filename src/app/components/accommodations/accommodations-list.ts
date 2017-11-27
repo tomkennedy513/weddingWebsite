56ad0547b3ee132102beaadb265cdea78ddd15f2
Merritt <merem02@ca.com>
2017-11-26 22:41:24 -0500

    pictures are in

--- src/app/components/accommodations/accommodations-list.ts
+++ src/app/components/accommodations/accommodations-list.ts
@@ -4,7 +4,7 @@ export const accommodations: [Accommodation] = [
 
     {
         name: "The Montauk Yacht Club",
-        photoUrl: "",
+        photoUrl: "https://www.herecomestheguide.com/images/venues_large/MontaukYachtClub2.jpg",
         aboutParagraph: "Book your stay at our beautiful reception venue!",
         address: "32 Star Island Rd, Montauk, NY 11954",
         phone: "631-668-3100",
@@ -13,7 +13,7 @@ export const accommodations: [Accommodation] = [
     },
     {
         name: "The Montauk Manor",
-        photoUrl: "",
+        photoUrl: "https://www.everettpotter.com/wp-content/uploads/2013/08/mmmanorfront.jpg",
         aboutParagraph:"Gorgeous Manor on a hill with beautiful views.",
         address:"236 Edgemere St, Montauk, NY 11954",
         phone: "631-668-4400",
@@ -22,8 +22,8 @@ export const accommodations: [Accommodation] = [
 
     },
     {
-        name:"Snug Harbor Motel",
-        photoUrl:"",
+        name:"Snug Harbor Motel and Marina",
+        photoUrl:"http://www.atlanticcruisingclub.com/Content/MarinaImages/25-376-5.jpg",
         aboutParagraph:"A short 5 minute walk from the venue! Our room block will be available for booking in the spring.",
         address: "3 Star Island Rd, Montauk, NY 11954",
         phone: "631-668-2860",
@@ -33,7 +33,7 @@ export const accommodations: [Accommodation] = [
     },
     {
         name:"Sun N' Sound",
-        photoUrl:"",
+        photoUrl:"https://edge.media.datahc.com/HI291269728.jpg",
         aboutParagraph:"",
         address:"22 Soundview Dr, Montauk, NY 11954",
         phone:"631-668-2212",
@@ -42,7 +42,7 @@ export const accommodations: [Accommodation] = [
     },
     {
         name:"Harborside Resort Motel",
-        photoUrl:"",
+        photoUrl:"https://media-cdn.tripadvisor.com/media/photo-s/0c/91/74/41/harborside-resort-motel.jpg",
         aboutParagraph:"A short 10 Minute walk from our venue!",
         address:"371 W Lake Dr, Montauk, NY 11954",
         phone:"631-668-2511",
@@ -51,7 +51,7 @@ export const accommodations: [Accommodation] = [
     },
     {
         name:"Airbnb",
-        photoUrl:"",
+        photoUrl:"https://static.dezeen.com/uploads/2014/07/Airbnb-rebrand-by-DesignStudio_dezeen_468_8.jpg",
         aboutParagraph:"",
         address:"",
         phone:"",
