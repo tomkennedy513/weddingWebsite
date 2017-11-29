cb6d1508363c7b6dbcadc0854f3b88a8b7d0e3c2
Tom Kennedy <kennedy513@gmail.com>
2017-11-29 11:34:14 -0500

    working on gallery

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -14,28 +14,71 @@ export class GalleryComponent implements OnInit {
 
 
   images = [
-    {
-      src: '/assets/img/gallery/IMG_0631.JPG'
-    },
-    {
-      src: '/assets/img/gallery/IMG_0632.JPG'
-    },
-    {
-      src: '/assets/img/gallery/IMG_0633.JPG'
-    },
-    {
-      src: '/assets/img/gallery/IMG_0634.JPG'
-    },
-    {
-      src: '/assets/img/gallery/IMG_0636.JPG'
-    },
-    {
-      src: '/assets/img/gallery/IMG_0637.JPG'
-    },
-    {
-      src: '/assets/img/gallery/IMG_0638.JPG'
-    }
-    ];
+      {
+        src: '/assets/img/gallery/IMG_0631.JPG'
+      },
+      {
+        src: '/assets/img/gallery/IMG_0632.JPG'
+      },
+      {
+        src: '/assets/img/gallery/IMG_0633.JPG'
+      },
+      {
+        src: '/assets/img/gallery/IMG_0634.JPG'
+      },
+      {
+        src: '/assets/img/gallery/IMG_0636.JPG'
+      },
+      {
+        src: '/assets/img/gallery/IMG_0637.JPG'
+      },
+      {
+        src: '/assets/img/gallery/IMG_0638.JPG'
+      },
+      {
+          src: '/assets/img/gallery/IMG_1545.jpg'
+      },
+      {
+          src: '/assets/img/gallery/jan 2015.JPG'
+      },
+      {
+          src: '/assets/img/gallery/jan 2016.jpeg'
+      }, 
+      {
+          src: '/assets/img/gallery/mar 2016+.jpg'
+      },
+      {
+          src: '/assets/img/gallery/march 2015.jpg'
+      },
+      {
+          src: '/assets/img/gallery/May 2013.JPG'
+      },
+      {
+          src: '/assets/img/gallery/May 2014.JPG'
+      },
+      {
+          src: '/assets/img/gallery/May 2016.jpg'
+      },
+      {
+          src: '/assets/img/gallery/nov 2016.JPG'
+      },
+      {
+          src: '/assets/img/gallery/nov 2016 2.JPG'
+      },
+      {
+          src: '/assets/img/gallery/oct 2013.JPG'
+      },
+      {
+          src: '/assets/img/gallery/oct 2017.JPG'
+      },
+      {
+          src: '/assets/img/gallery/sept 2015.jpg'
+      },
+      {
+          src: '/assets/img/gallery/september 2012.JPG'
+      }
+      
+  ];
 
   ngOnInit() {
 
