37ee8c901e128a829dabb00f8cde76c41dc0d95e
Tom Kennedy <kennedy513@gmail.com>
2018-02-23 15:34:48 -0500

    added vrbo

--- src/app/components/accommodations/accommodations-list.ts
+++ src/app/components/accommodations/accommodations-list.ts
@@ -14,48 +14,57 @@ export const accommodations: [Accommodation] = [
     {
         name: "The Montauk Manor",
         photoUrl: "https://www.everettpotter.com/wp-content/uploads/2013/08/mmmanorfront.jpg",
-        aboutParagraph:"Gorgeous Manor on a hill with beautiful views.",
-        address:"236 Edgemere St, Montauk, NY 11954",
+        aboutParagraph: "Gorgeous Manor on a hill with beautiful views.",
+        address: "236 Edgemere St, Montauk, NY 11954",
         phone: "631-668-4400",
-        website:"http://www.montaukmanor.com/",
-        blocked:true
+        website: "http://www.montaukmanor.com/",
+        blocked: true
 
     },
     {
-        name:"Snug Harbor Motel and Marina",
-        photoUrl:"http://www.atlanticcruisingclub.com/Content/MarinaImages/25-376-5.jpg",
-        aboutParagraph:"A short 5 minute walk from the venue! Our room block will be available for booking in the spring.",
+        name: "Snug Harbor Motel and Marina",
+        photoUrl: "http://www.atlanticcruisingclub.com/Content/MarinaImages/25-376-5.jpg",
+        aboutParagraph: "A short 5 minute walk from the venue! Our room block will be available for booking in the spring.",
         address: "3 Star Island Rd, Montauk, NY 11954",
         phone: "631-668-2860",
-        website:"http://www.montauksnugharbor.com/",
+        website: "http://www.montauksnugharbor.com/",
         blocked: false
-    
+
     },
     {
-        name:"Sun N' Sound",
-        photoUrl:"https://edge.media.datahc.com/HI291269728.jpg",
-        aboutParagraph:"",
-        address:"22 Soundview Dr, Montauk, NY 11954",
-        phone:"631-668-2212",
-        website:"https://www.duneresorts.com/resorts/sun-n-sound/",
+        name: "Sun N' Sound",
+        photoUrl: "https://edge.media.datahc.com/HI291269728.jpg",
+        aboutParagraph: "",
+        address: "22 Soundview Dr, Montauk, NY 11954",
+        phone: "631-668-2212",
+        website: "https://www.duneresorts.com/resorts/sun-n-sound/",
         blocked: false
     },
     {
-        name:"Harborside Resort Motel",
-        photoUrl:"https://media-cdn.tripadvisor.com/media/photo-s/0c/91/74/41/harborside-resort-motel.jpg",
-        aboutParagraph:"A short 10 Minute walk from our venue!",
-        address:"371 W Lake Dr, Montauk, NY 11954",
-        phone:"631-668-2511",
-        website:"http://www.montaukharborside.com/",
+        name: "Harborside Resort Motel",
+        photoUrl: "https://media-cdn.tripadvisor.com/media/photo-s/0c/91/74/41/harborside-resort-motel.jpg",
+        aboutParagraph: "A short 10 Minute walk from our venue!",
+        address: "371 W Lake Dr, Montauk, NY 11954",
+        phone: "631-668-2511",
+        website: "http://www.montaukharborside.com/",
         blocked: true
     },
     {
-        name:"Airbnb",
-        photoUrl:"https://static.dezeen.com/uploads/2014/07/Airbnb-rebrand-by-DesignStudio_dezeen_468_8.jpg",
-        aboutParagraph:"",
-        address:"",
-        phone:"",
-        website:"https://www.airbnb.com/s/Montauk--NY--United-States/all?place_id=ChIJ9w76UFKr6IkRT2G_wWK3LgE&refinement_path=%2Ffor_you",
+      name: "VRBO",
+      photoUrl: "https://crunchbase-production-res.cloudinary.com/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/v1482918028/oi1pbensackysmc1gyku.png",
+      aboutParagraph: "",
+      address: "",
+      phone: "",
+      website: "https://www.vrbo.com/results?pets=false&q=Montauk%2C+NY%2C+USA&to-date=09%2F23%2F2018&children=0&from-date=09%2F21%2F2018&adults=0&uuid=",
+      blocked: false
+    },
+    {
+        name: "Airbnb",
+        photoUrl: "https://static.dezeen.com/uploads/2014/07/Airbnb-rebrand-by-DesignStudio_dezeen_468_8.jpg",
+        aboutParagraph: "",
+        address: "",
+        phone: "",
+        website: "https://www.airbnb.com/s/Montauk--NY--United-States/all?place_id=ChIJ9w76UFKr6IkRT2G_wWK3LgE&refinement_path=%2Ffor_you",
         blocked: false
     }
-]
+];
