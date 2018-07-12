52537298c20153abf864b90e7d84d7e17586c96d
Tom Kennedy <kennedy513@gmail.com>
2018-07-12 14:07:44 -0400

    updated registry

--- src/app/components/registry/registry.component.ts
+++ src/app/components/registry/registry.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-registry',
   templateUrl: './registry.component.html',
   styleUrls: ['./registry.component.css']
 })
-export class RegistryComponent implements OnInit {
+export class RegistryComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
 
+  }
+
+  ngAfterViewInit(): void {
+    const s = document.createElement("script");
+    s.id = 'script_myregistry_giftlist_iframe';
+    s.type = 'text/javascript';
+    s.src = '//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=4e7gjY5Dq4xswOIdeZHvIw2&v=2';
+    this.elementRef.nativeElement.appendChild(s);
   }
 
 }
